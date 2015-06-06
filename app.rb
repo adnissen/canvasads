c2ee56002c805a9ede4c37de7c0fb8d97d6cef26
adnissen <andrew_nissen@yahoo.com>
2015-06-05 20:09:31 -0400

    that's what I get for not writing a spec

--- app.rb
+++ app.rb
@@ -212,6 +212,6 @@ get '/dashboard' do
   return 406 unless admin?
 
   "total impressions: #{Ad.total_impressions}\n
-  total unfilled: #{Total.total_unfilled}\n
-  fill rate: #{100 - ((Total.total_unfilled / Ad.total_impressions) * 100)}"
+  total unfilled: #{Token.total_unfilled}\n
+  fill rate: #{100 - ((Token.total_unfilled / Ad.total_impressions) * 100)}"
 end
