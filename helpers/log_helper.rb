a320ac9f5bd86984bb613341cdfe3ee08e95b23c
tucker <gtgett@gmail.com>
2015-06-07 15:51:51 -0400

    added log functions

---
+++ helpers/log_helper.rb
@@ -0,0 +1,164 @@
+#
+# This file contains the helper functions for the server logs. Choose the function by the type of event being logged.
+#
+# @author: gtgettel
+
+require 'builder'
+require 'Nokogiri'
+
+#
+# Creates a log file under 'helpers/logs/'
+#
+# @params: filename - full name of log file (use .xml extension) e.g. master_log.xml
+# @params: log_type - description of events that the log will hold
+# @return: N/A
+#
+def log_create(filename, log_type)
+	path = "helpers/logs/" + filename
+	file = File.new(path, "w+")
+	xml_file = Builder::XmlMarkup.new(:target => file, :indent => 1)
+	xml_file.instruct! :xml, :encoding => "ASCII" # adds xml encoding info
+	xml_file.log(Time.now.strftime("%d/%m/%Y %H:%M"), "type" => log_type) # creates first entry with date log opened and log_type
+end
+
+#
+# Creates a log of an impression in the master_log.xml
+#
+# @params: request - sinatra request served to route handler
+# @params: ad_id - id of add being returned
+# @return: N/A
+#
+def log_Impression(request, ad_id)
+	File.open("helpers/logs/master_log.xml", "a") { |file|
+		xml_file = Builder::XmlMarkup.new(:target => file, :indent => 2)
+		xml_file.impression { |imp|  # creates XML entry
+			imp.time(Time.now.strftime("%d/%m/%Y %H:%M")); 
+			imp.token(request['token']); 
+			imp.host(request.host); 
+			imp.ip(request.ip); 
+			imp.adID(ad_id) 
+		}
+
+			# <impression>
+			# => <time>   	</time>
+			# => <token>   	</token>
+			# => <host>		</host>
+			# => <ip>		</ip>
+			# => <adID>		</adID>
+			#</impression>
+	}
+end
+
+#
+# Creates a log of a request that went unserved in the master_log.xml
+# 
+# @params: request - sinatra request served to route handler
+# @return: N/A
+#
+def log_NoServe(request)
+	File.open("helpers/logs/master_log.xml", "a") { |file|
+	xml_file = Builder::XmlMarkup.new(:target => file, :indent => 2)
+		xml_file.noServe { |ns|  # creates XML entry
+			ns.time(Time.now.strftime("%d/%m/%Y %H:%M"));
+			ns.token(request['token']); 
+			ns.host(request.host); 
+			ns.ip(request.ip); 
+		}
+
+			# <noServe>
+			# => <time>   	</time>
+			# => <token>   	</token>
+			# => <host>		</host>
+			# => <ip>		</ip>
+			#</noServe>
+	}
+end
+
+#
+# Counts the number of impressions in log file
+#
+# @params: logfile - xml file that contains impression log
+# @return: counter - number of impressions in file
+# 
+def count_Impressions(logfile)
+	file = File.open("helpers/logs/" + logfile)
+	doc = Nokogiri::XML(file)
+	counter = 0
+	doc.xpath('/impression').each do |imp| # gets each <impression> tag
+		counter++
+	end
+	return counter
+end
+
+#
+# Counts the number of noServes in log file
+#
+# @params: logfile - xml file that contains noServe log
+# @return: counter - number of noServes in file
+#
+def count_noServe(logfile)
+	file = File.open("helpers/logs/" + logfile)
+	doc = Nokogiri::XML(file)
+	counter = 0
+	doc.xpath('/noServe').each do |imp| # gets each <noServe> tag
+		counter++
+	end
+	return counter
+end
+
+#
+# Counts the number of impressions in log file by nostname
+#
+# @params: logfile - xml file that contains impression log
+# @params: hostname - host name of site that requested ad
+# @return: counter - number of impressions in file
+# 
+def count_Impressions_byHost(logfile, hostname)
+	file = File.open("helpers/logs/" + logfile)
+	doc = Nokogiri::XML(file)
+	counter = 0
+	doc.xpath('/impression').each do |imp| # gets each <impression> tag
+		if imp.at_xpath('host') == hostname
+			counter++
+		end
+	end
+	return counter
+end
+
+#
+# Counts the number of noServes in log file by hostname
+#
+# @params: logfile - xml file that contains noServe log
+# @params: hostname - host name of site that requested ad
+# @return: counter - number of noServes in file
+#
+def count_noServe_byHost(logfile, hostname)
+	file = File.open("helpers/logs/" + logfile)
+	doc = Nokogiri::XML(file)
+	counter = 0
+	doc.xpath('/noServe').each do |ns| # gets each <noServe> tag
+		if ns.at_xpath('host') == hostname
+			counter++
+		end
+	end
+	return counter
+end
+
+#
+# Counts the number of impressions in log file by ad ID
+#
+# @params: logfile - xml file that contains impression log
+# @params: ad_ID - ID of requested ad
+# @return: counter - number of impressions in file
+# 
+def count_Impressions_byAdID(logfile, ad_ID)
+	file = File.open("helpers/logs/" + logfile)
+	doc = Nokogiri::XML(file)
+	counter = 0
+	doc.xpath('/impression').each do |imp| # gets each <impression> tag
+		if imp.at_xpath('adID') == ad_ID
+			counter++
+		end
+	end
+	return counter
+end
